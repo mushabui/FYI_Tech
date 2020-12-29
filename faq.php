cca1a60e0b6da281a600039913db7d02e9f7556c
fajarnurrahman09 <62900256+fajarnurrahman09@users.noreply.github.com>
2020-12-30 02:07:00 +0700

    Add files via upload

---
+++ faq.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="css/style1.css" />
+
+    <link rel="icon" type="img/png" href="img/fyi_logotitle.png" />
+    <title img src="img/fyi_logotitle.png">Fyi_Tech</title>
+</head>
+<body>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+
+
+  <nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="index.php"><img src="img/fyi_logo1.png" alt="FYI_TECH" width= "150px"></a>
+    <form class="form-inline">
+      <input class="form-control mr-sm-2" type="search" placeholder="Cari Produk" aria-label="Search">
+      <button class="btn btn-primary my-2 my-sm-0" type="submit">Cari</button>
+      <button type="button" class="btn btn-light" href="login.php"><a href="login.php">Login</a></button>
+    </form>
+  </nav>
+
+    <nav class="navbar1">
+        <div class="container">
+            <ul class="nav nav-pills nav-justified">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Branda</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="daftarP.php">Daftar Produk</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="tipsdantrik.php">Tips dan Trik</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="tentang.php">Tentang</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="faq.php">FAQ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="forumdiskusi.php">Forum Diskusi</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container">
+    <nav class="navbar nav justify-content-center">
+        <a class="navbar-brand" href="index.php"><img src="img/fyi_logo.png" alt="FYI_TECH" width= "200px"></a>
+    </nav>
+</div>
+<div class="container">
+    <h1>FAQ</h1>
+    <div class="row">
+        <div class="accordion" id="accordionExample">
+            <div class="card">
+              <div class="card-header" id="headingOne">
+                <h2 class="mb-0">
+                  <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    Collapsible Group Item #1
+                  </button>
+                </h2>
+              </div>
+          
+              <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+                <div class="card-body">
+                  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                </div>
+              </div>
+            </div>
+            <div class="card">
+              <div class="card-header" id="headingTwo">
+                <h2 class="mb-0">
+                  <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    Collapsible Group Item #2
+                  </button>
+                </h2>
+              </div>
+              <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                <div class="card-body">
+                  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                </div>
+              </div>
+            </div>
+            <div class="card">
+              <div class="card-header" id="headingThree">
+                <h2 class="mb-0">
+                  <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    Collapsible Group Item #3
+                  </button>
+                </h2>
+              </div>
+              <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                <div class="card-body">
+                  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                </div>
+              </div>
+            </div>
+          </div>
+    </div>
+</div>
+
+  
+        
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,116 @@
+<?php
+if( isset($_POST["submit"]) ){
+    if( $_POST["username"] == "admin" && $_POST["password"] == "fajarijal") {
+        header("Location: admin.php");
+        exit;
+    } else {
+        $error = true;
+    }
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="css/style1.css"/>
+
+    <link rel="icon" type="img/png" href="img/fyi_logotitle.png"   />
+    <title img src="img/fyi_logotitle.png">Fyi_Tech</title>
+</head>
+<body>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+
+
+    <nav class="navbar navbar-light bg-light">
+        
+        <a class="navbar-brand" href="index.html"><img src="img/fyi_logo1.png"alt="FYI_TECH" width= "150px"></a>
+        <form class="form-inline">
+        <input class="form-control mr-sm-2" type="search" placeholder="Cari Produk" aria-label="Search">
+        <button class="btn btn-primary my-2 my-sm-0" type="submit">Cari</button>
+        <button type="button" class="btn btn-light" href="/login"><a href="/login">Login</a></button>
+        </form>
+        
+    </nav>
+
+    <nav class="navbar1">
+        <div class="container">
+            <ul class="nav nav-pills nav-justified">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Beranda</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="daftarP.php">Daftar Produk</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="tipsdantrik.php">Tips dan Trik</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="tentang.php">Tentang</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="faq.php">FAQ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="forumdiskusi.php">Forum Diskusi</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+
+</body>
+</html>
+
+<div class="container ">
+  <nav class="navbar nav justify-content-center">
+    <a class="navbar-brand" href="index.php"><img src="img/fyi_logo.png" alt="FYI_TECH" width= "150px"></a>
+  </nav>
+</div>
+
+<div class="container ">
+  <nav class="navbar nav justify-content-center">
+      <span class="navbar-brand mb-0 h1">Login Admin</span>
+  </nav>
+</div>
+
+<div class="container border">
+	<div class="row">
+		<div class="col-lg-4"></div>
+		<div class="col-lg-4">
+			<form action="/login/handle" method="POST">
+    <div class="form-group">
+      <label for="exampleInputEmail1">Username</label>
+      <input name="username" type="text" class="form-control" id="exampleInputUsername1" aria-describedby="emailHelp">
+    </div>
+    <div class="form-group ">
+      <label for="exampleInputPassword1">Password</label>
+      <input name="password" type="password" class="form-control" id="exampleInputPassword1">
+    </div>
+      <nav class="navbar nav justify-content-center"></nav>
+        <button type="submit" class="btn btn-primary " href="/admin">Login</button>
+      </nav>
+  </form>
+		</div>
+		<div class="col-lg-4"></div>
+	</div>
+	
+</div>
+
