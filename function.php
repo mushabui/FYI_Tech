26e47cf49eba773b8eeb068679c6f56a7f8981ed
Mushab Utsmani <62864891+mushabui@users.noreply.github.com>
2021-01-03 15:04:52 +0700

    Add files via upload

---
+++ function.php
@@ -0,0 +1,10 @@
+<?php 
+ 
+$function = mysqli_connect("localhost","root","","fyi_tech");
+ 
+// Check connection
+if (mysqli_connect_errno()){
+	echo "Koneksi database gagal : " . mysqli_connect_error();
+}
+
+?>
