2e023938e88d5e92b88d411ce029eb182a9d8579
Mushab Utsmani <62864891+mushabui@users.noreply.github.com>
2021-01-05 10:57:44 +0700

    Add files via upload

--- daftarP.php
+++ daftarP.php
@@ -1,73 +1,12 @@
-<?php 
-// $_GET
-$laptop = [
-	[	
-		"nama" => "hp 15-db0011au",
-		"cpu" => "Ryzen 3 2200u",
-		"vga" => "vega 3",
-        "layar" => "15'",
-        "harga" => "5.000.000,00",
-        "link" => ""
-	],
-	[
-		"nama" => "lenovo ideapad 330s ",
-		"cpu" => "Ryzen 5 2500u",
-		"vga" => "vega 8",
-        "layar" => "14'",
-        "harga" => "5.000.000,00",
-        "link" => ""
-    ],
-    [
-		"nama" => "lenovo ideapad 22s ",
-		"cpu" => "Ryzen 5 2500u",
-		"vga" => "vega 8",
-        "layar" => "14'",
-        "harga" => "5.000.000,00",
-        "link" => ""
-    ],
-    [
-		"nama" => "lenovo ideapad 3800s ",
-		"cpu" => "Ryzen 5 2500u",
-		"vga" => "vega 8",
-        "layar" => "14'",
-        "harga" => "5.000.000,00",
-        "link" => ""
-    ],
-    [	
-		"nama" => "hp 15-db0011au",
-		"cpu" => "Ryzen 3 2200u",
-		"vga" => "vega 3",
-        "layar" => "15'",
-        "harga" => "5.000.000,00",
-        "link" => "https://www.tokopedia.com/toko-gadgetz/laptop-hp-15-db0011au-amd-ryzen-3-vega-3-ram4gb-hdd500gb-like-new?refined=true&whid=0"
-	],  
-	[
-		"nama" => "lenovo ideapad 330s ",
-		"cpu" => "Ryzen 5 2500u",
-		"vga" => "vega 8",
-        "layar" => "14'",
-        "harga" => "5.000.000,00",
-        "link" => ""
-    ],
-    [
-		"nama" => "lenovo ideapad 22s ",
-		"cpu" => "Ryzen 5 2500u",
-		"vga" => "vega 8",
-        "layar" => "14'",
-        "harga" => "5.000.000,00",
-        "link" => ""
-    ],
-    [
-		"nama" => "lenovo ideapad 3800s ",
-		"cpu" => "Ryzen 5 2500u",
-		"vga" => "vega 8",
-        "layar" => "14'",
-        "harga" => "5.000.000,00",
-        "link" => ""
-        
-	]
-];
+<?php
+session_start();
+
+// menghubungkan dengan koneksi
+include 'function.php';
+
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -89,13 +28,10 @@ $laptop = [
     <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-
-    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
-    -->
-
+    
     <nav class="navbar navbar-light bg-light">
     <a class="navbar-brand" href="index.php"><img src="img/fyi_logo1.png" alt="FYI_TECH" width= "150px"></a>
     <form class="form-inline">
@@ -109,7 +45,7 @@ $laptop = [
         <div class="container">
             <ul class="nav nav-pills nav-justified">
                 <li class="nav-item">
-                    <a class="nav-link" href="index.php">Branda</a>
+                    <a class="nav-link" href="index.php">Beranda</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link " href="daftarP.php">Daftar Produk</a>
@@ -128,35 +64,12 @@ $laptop = [
     </nav>
     </div>
 
-    <div class="container ">
-        <nav class="navbar nav justify-content-center">
-            <span class="navbar-brand mb-0 h1">Daftar Laptop</span>
-        </nav>
-    </div>
-
-    <div class="container border">
-    <div class="container ">
-        <nav class="navbar nav justify-content-center">
-            <span class="navbar-brand mb-0 h1">Compare Laptop</span>
-        </nav>
-    </div>
-       <div class="row">
-           <div class="col-lg-2"></div>
-           <div class="col-lg-3"><img src="img/compare1.jpg" alt="FYI_TECH" width= "100%"><button type="button" class="btn btn-light">Tambah</button></div>
-           <div class="col-lg-2"></div>
-           <div class="col-lg-3"><img src="img/compare2.jpg" alt="FYI_TECH" width= "100%"><button type="button" class="btn btn-light">Tambah</button></div>
-           <div class="col-lg-2"></div>
-        </div>
-        
-        <nav class="navbar nav justify-content-center">
-	        <button type="button" class="btn btn-primary btn-sm">Compare</button>
-        </nav>
-        
-    </div>
+    
+    
 
     <div class="container">
         <nav class="navbar navbar-light bg-white">
-            <a class="navbar-brand" href="index.php"><img src="img/fyi_logo1.png" alt="FYI_TECH" width= "150px"></a>
+            <span class="navbar-brand mb-0 h1">Daftar Laptop</span>
             <form class="form-inline">
             <input class="form-control mr-sm-2" type="search" placeholder="Kategori" aria-label="Search">
             <button class="btn btn-primary my-2 my-sm-0" type="submit">Cari</button>
@@ -165,51 +78,48 @@ $laptop = [
     </div>
 
     <div class="container border">
-        <nav class="navbar nav justify-content-center">
-            <span class="navbar-brand mb-0 h1">Daftar Laptop</span>
-        </nav>
+        
         
         <div class=row style:"padding:90px">
-        <?php foreach( $laptop as $ltp ) : ?>
+            <?php 
+                include 'function.php';
+                
+		        $data = mysqli_query($function,"SELECT * FROM produk");
+		        while($d = mysqli_fetch_array($data)){
+		            ?>
             <div class="card col-lg-3" >
-                <img src="..." class="card-img-top" alt="...">
+                
+                <img src="<?php echo $d['gambar']; ?>" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title"><a href="latihan2.php?nama=<?= $ltp["nama"]; ?>&cpu=<?= $ltp["cpu"]; ?>&vga=<?= $ltp["vga"]; ?>&layar=<?= $ltp["layar"]; ?>"><?= $ltp["nama"]; ?></a></h5>
+                    <h5 class="card-title"><a href=""></a></h5>
                     
                 </div>
+                
+
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item">CPU   :<?= $ltp["cpu"]; ?></li>
-                    <li class="list-group-item">VGA   :<?= $ltp["vga"]; ?></li>
-                    <li class="list-group-item">LAYAR :<?= $ltp["layar"]; ?></li>
-                    
+                    <li class="list-group-item"><?php echo $d['nama']; ?></li>
+                    <li class="list-group-item">CPU   : <?php echo $d['cpu']; ?></li>
+                    <li class="list-group-item">VGA   : <?php echo $d['vga']; ?></li> 
+                    <li class="list-group-item">RAM : <?php echo $d['ram']; ?></li>
+                    <li class="list-group-item">LAYAR : <?php echo $d['layar']; ?></li>
                 </ul>
+                
+			        
+			        
                 <div class="card-body">
-                <li class="list-group-item" style="background-colour:red">harga :<?= $ltp["harga"]; ?></li>
-                    <a href="<?= $ltp["link"];?>"class="card-link">Cek Toko</a>
+                <ul><li class="list-group-item" style="background-colour:red" >Rp. <?php echo $d['harga']; ?></li></ul>
+                   
                 </div>
             </div>
-            <?php endforeach; ?>
-            </div>
 
-	    <!-- <div class="row">
-            <div class="col-lg-2"></div>
-
-            <div class="col-lg-2"><img src="img/prodak/c1.png" alt="FYI_TECH" width= "100%"></div>
-            <div class="col-lg-2"><img src="img/prodak/c2.png" alt="FYI_TECH" width= "100%"></div>
-            <div class="col-lg-2"><img src="img/prodak/c3.png" alt="FYI_TECH" width= "100%"></div>
-            <div class="col-lg-2"><img src="img/prodak/c4.png" alt="FYI_TECH" width= "100%"></div>
             
-            <div class="col-lg-2"></div>
-      </div>
-      <div class="row">
-        <div class="col-lg-2"></div>
-
-        <div class="col-lg-2"><img src="img/prodak/c1.png" alt="FYI_TECH" width= "100%"></div>
-        <div class="col-lg-2"><img src="img/prodak/c2.png" alt="FYI_TECH" width= "100%"></div>
-        <div class="col-lg-2"><img src="img/prodak/c3.png" alt="FYI_TECH" width= "100%"></div>
-        <div class="col-lg-2"><img src="img/prodak/c4.png" alt="FYI_TECH" width= "100%"></div>
+                <?php 
+		    }
+		    ?>
         
-        <div class="col-lg-2"></div> -->
+         </div>
+
+	   
   </div>
     </div>
 
